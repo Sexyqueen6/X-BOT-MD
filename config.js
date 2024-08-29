36b9d4fa46cafa11f9cebe3a01f9c4108432b4df
Sexyqueen6 <botlisa36@gmail.com>
2024-08-29 07:32:09 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:d24c8a71335ac901db0f80711c858fc7";
+const SUDO = process.env.SUDO || '18768375254';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const DISABLE_PM = process.env.DISABLE_PM  || "false";
